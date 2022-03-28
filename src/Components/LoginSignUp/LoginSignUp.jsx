47a3eaa33e28b60a13e4856e45a7675a375e8546
shyam g <meghashyam.g@gmail.com>
2022-03-28 16:56:42 +0530

    u5c4

--- src/Components/LoginSignUp/LoginSignUp.jsx
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,13 +1,92 @@
+import axios from "axios";
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import { store } from "../../Redux/store";
+import {useSelector, useDispatch} from "../../Redux/Login/action.js"
+import { Link, Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 export const LoginSignUp = () => {
+  const [userSU, setUserSU] = useState({
+    name: "",
+    password: "",
+    location: "",
+    interests: [],
+    image: ""
+  });
+  const dispatch = useDispatch();
+  const logData  = useSelector((store) => store.user);
+  const handleChange = (event) => {
+    const {id, value} = event.target;
+    setUserSU({
+      ...userSU, 
+      [id] : value
+    });
+    console.log(userSU);
+  }
+  const handleChck = (event) => {
+    const {value} = event.target;
+    userSU.interests.push(value);
+    console.log(userSU);
+  }
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8080/users", {...userSU, id: nanoid()})
+    .then(res => {console.log(res)})
+    .catch((error) => {
+      console.log(error);
+      alert("Sign Up Successful!");
+    })
+  }
+const [userSI, setUserSI] = useState({
+  name: "",
+  password: ""
+})
+
+  const [users, storeUsers] = useState([]);
+
+  const handleChangeSI = (event) => {
+    const {id, value} = event.target;
+    setUserSI({
+      ...userSI, 
+      [id] : value
+    });
+    console.log(userSI);
+  }
+  const getUsers = () => {
+    axios.get("http://localhost:8080/users")
+    .then(res => {
+      storeUsers(res);
+    })
+  };
+  let navigate = useNavigate();
+  const handleSignin = (e) => {
+    const getUsers = () => {
+      axios.get("http://localhost:8080/users")
+      .then(res => {
+        storeUsers(res);
+      })
+    };
+    users.map((el) => {
+      if(el.name===userSI.name && el.password===userSI.password){
+        alert("Signed In");
+        navigate("/");
+      }
+    })
+  }
+
+
   return (
     <div className="loginSignUp">
-      <form className="signUp" onSubmit={(e) => { }}>
+      <form className="signUp" onSubmit={(e) => {handleSignup(e) }}>
         <h1>SignUp</h1>
         <label>name</label>
         <input
           type="text"
           className="name"
-          onChange={(event) => { }}
+          id="name"
+          onChange={(event) => {handleChange(event) }}
           required
         />
         <br />
@@ -15,58 +94,61 @@ export const LoginSignUp = () => {
         <input
           type="text"
           className="password"
-          onChange={(event) => { }}
+          id="password"
+          onChange={(event) => {handleChange(event) }}
           required
         />
         <br />
-        <select value={""} className="location" onChange={(event) => { }}>
+        <select value={""} className="location" id="location" onChange={(event) => {handleChange(event) }}>
           <option value=""></option>
           <option value="bangalore">Bangalore</option>
           <option value="kolkata">Kolkata</option>
           <option value="delhi">Delhi</option>
           <option value="mumbai">Mumbai</option>
         </select>
+        <br />
         <label>Interests</label>
         <br />
         <label>technology</label>
         <input
-          type="checkbox"
+          type="checkbox" id="interest" value="technology"
           className="technology"
-          onChange={(event) => { }}
+          onChange={(event) => {handleChck(event) }}
         />
         <br />
         <label>food</label>
-        <input type="checkbox" className="food" onChange={(event) => { }} />
+        <input type="checkbox" id="interest" value="food" className="food" onChange={(event) => {  handleChck(event) }} />
         <br />
         <label>movies</label>
-        <input type="checkbox" className="movies" onChange={(event) => { }} />
+        <input type="checkbox" id="interest" value="movies" className="movies" onChange={(event) => {handleChck(event) }} />
         <br />
         <label>culture</label>
-        <input type="checkbox" className="culture" onChange={(event) => { }} />
+        <input type="checkbox" id="interest" value="culture" className="culture" onChange={(event) => {handleChck(event) }} />
         <br />
         <label>art</label>
-        <input type="checkbox" className="art" onChange={(event) => { }} />
+        <input type="checkbox" id="interest" value="art" className="art" onChange={(event) => { handleChck(event)}} />
         <br />
         <label>drama</label>
-        <input type="checkbox" className="drama" onChange={(event) => { }} />
+        <input type="checkbox" id="interest" value="drama" className="drama" onChange={(event) => { handleChck(event)}} />
         <br />
         <label>image</label>
         <input
-          type="text"
+          type="url"
           className="image"
-          onChange={(event) => { }}
+          id="image"
+          onChange={(event) => {handleChange(event) }}
           required
         />
         <br />
         <input type="submit" className="submitSignUpForm" />
       </form>
-      <form className="login" onSubmit={(e) => { }}>
+      <form className="login" onSubmit={(e) => {handleSignin(e) }}>
         <h1>Login</h1>
         <label>name</label>
         <input
           type="text"
           className="name"
-          onChange={(event) => { }}
+          onChange={(event) => {handleChangeSI(event) }}
           required
         />
         <br />
@@ -74,7 +156,7 @@ export const LoginSignUp = () => {
         <input
           type="text"
           className="password"
-          onChange={(event) => { }}
+          onChange={(event) => { handleChangeSI(event)}}
           required
         />
         <br />
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
 import { loginReducer } from "./Login/loginReducer";
 import { combineReducers, createStore } from "redux";
 
-export const store = createStore(); // add your reducers here
+export const store = createStore(loginReducer, 0); // add your reducers here
 
